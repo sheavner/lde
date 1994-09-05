1818564ce8851da547c004f09e1d720c4211a946
sdh <sdh>
1994-09-05 22:57:42 +0000

    Initial revision

---
+++ src/curses.h
@@ -0,0 +1,13 @@
+/*
+ *  lde/curses.h -- The Linux Disk Editor
+ *
+ *  Copyright (C) 1994  Scott D. Heavner
+ *
+ *  $Id: curses.h,v 1.1 1994/09/05 22:57:42 sdh Exp $
+ */
+
+#ifdef NC_HEADER
+#include <ncurses.h>
+#else
+#include <curses.h>
+#endif
