b388b89eec512a98807be11e1b5fe4d08be0d8c4
sdh <sdh>
1994-09-05 23:06:27 +0000

    Initial revision

---
+++ src/nc_dir.h
@@ -0,0 +1,9 @@
+/*
+ *  lde/nc_dir.h -- The Linux Disk Editor
+ *
+ *  Copyright (C) 1994  Scott D. Heavner
+ *
+ *  $Id: nc_dir.h,v 1.1 1994/09/05 23:06:27 sdh Exp $
+ */
+
+int directory_popup(unsigned long bnr);
