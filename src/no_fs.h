a3a3201657409727a740b7d92678971dd158217d
sdh <sdh>
1996-10-12 20:50:09 +0000

    Added NOFS_get_device_size()

--- src/no_fs.h
+++ src/no_fs.h
@@ -3,13 +3,15 @@
  *
  *  Copyright (C) 1994  Scott D. Heavner
  *
- *  $Id: no_fs.h,v 1.3 1995/06/01 05:55:48 sdh Exp $
+ *  $Id: no_fs.h,v 1.4 1996/10/12 20:50:09 sdh Exp $
  */
 
 struct Generic_Inode *NOFS_init_junk_inode(void);
 void NOFS_init(char * sb_buffer);
 int NOFS_null_call(void);
 int NOFS_one(unsigned long nr);
+unsigned long NOFS_get_device_size(int fd, int blocksize);
+
 
 
 
