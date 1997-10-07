5608b9175ecfc06246d21f16c85416c6f0da618d
sdh <sdh>
1997-10-07 14:50:51 +0000

    *** empty log message ***

--- crash_recovery/find_ext2_fs.c
+++ crash_recovery/find_ext2_fs.c
@@ -18,8 +18,12 @@
  * Otherwise, it will print out any blocks which contain EXT2_MAGIC, which may or
  * may not be superblocks.  Also, there are many copies of the super block on 
  * each filesystem, they will all yield the same size, but will have different
- * starts and ends.  It may be necessary to leave out the best guess option if
- * nothing shows up on the first run.
+ * starts and ends.  The first one should show the actual start and end of the
+ * partition, the rest think that the partition begins just before this copy of
+ * the superblock, so the start and end will be incorrect.  It may be necessary
+ * to leave out the best guess option if nothing shows up on the first run.
+ *
+ * $Id: find_ext2_fs.c,v 1.2 1997/10/07 14:50:51 sdh Exp $
  *
  * Changes:
  *
@@ -29,6 +33,21 @@
  * - comment out EXT2_PRE_02B_MAGIC - must not be supported in kernels anymore
  * - compile with "gcc -O6 -s -o find_ext2_fs find_ext2_fs.c" for a 4k binary
  * - why is this thing so slow?
+ *
+ * October 7, 1997 - try to speed things up by reading in more blocks at a
+ *   time
+ * - NB=32 is still painfully slow, maybe we should revert back to old version
+ *   that only read in 512 bytes at a time.  If NB=1, #ifdefs should revert
+ *   back to old version.
+ * - /usr/bin/time trials to search to ext2fs at sector 204624 of an ide disk:
+ *        NB=     32             8              1
+ *        real    1m17.916s      1m28.996s      1m40.012s   
+ *        user    0m0.200s       0m0.530s       0m0.490s
+ *        sys     0m57.270s      0m43.970s      0m49.060s
+ * - Might be nice to write a new tool that starts at block 1 of a disk,
+ *   figures out what's on it (msdos or linux partition), computes and displays
+ *   its size/start/end, the jumps to the end and looks for the next bit of
+ *   partition info.
  */ 
 
 /* The ext2 superblock begins 1024 bytes after the start of the ext2 file
@@ -36,19 +55,24 @@
  * of the super block */
 #define C -2UL
 
+/* Speed enhancement - set the number of 512 byte blocks we read at a time
+ * Feel free to twiddle this parameter, if you make it too big, performance
+ * probably won't improve - it will also control how close to the end of
+ * the partition you can get as the last NB sectors won't be checked, must be
+ * at least 1 */
+#define NB 32
+
 int main(int argc, char **argv)
 {
   unsigned long i=0UL, lastblock, last_lastblock=0UL, blocksize, last_size=0UL, ranch_size=0UL, ranch_last=0UL;
-  char *device_name="/dev/hdb";
+  char *device_name="/dev/hdb", buffer[NB*512];
   int fd, best_guess=0, this_is_it, j;
   struct ext2_super_block *sb;
-  
-  sb = malloc(1024);
-  if (!sb) {
-    printf("Out of memory - Woe is the machine without a spare kB!\n");
-    exit(-42);
-  }
-  
+
+#if NB<2
+  sb = (void *) buffer;
+#endif
+
   /* Parse command line arguments */
   if (argc==3) {
     best_guess = 1;
@@ -79,47 +103,56 @@ int main(int argc, char **argv)
   for(i=0UL;;) {
 
     /* Grab a block - dump it into memory defined as an ext2_super_block */
-    if ( read(fd, sb, 512) != 512 ) {
+    if ( read(fd, buffer, NB*512) != NB*512 ) {
       printf("Error reading block or EOF encountered (EOF is good)\n");
       exit(-2);
     }
-    
-    /* It probably isn't it */
-    this_is_it = 0;
-    
-    /* Check if the ext2 magic number is in the right place */
-    /* if ( (sb->s_magic== EXT2_SUPER_MAGIC)||(sb->s_magic == EXT2_PRE_02B_MAGIC) ) { */
-    if (sb->s_magic== EXT2_SUPER_MAGIC) {
 
-      /* When we get a match, figure out where this partiton would end */
-      blocksize = (unsigned long)EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
-      lastblock = (i+C)+(sb->s_blocks_count*(blocksize/512UL)-1UL); /* Start + (size-1): i.e. 100 block fs starting at block 0 runs 0->99 */
+#if NB>1
+    /* Operate on all the blocks we've read into memory */
+    for (j=0;j<NB;j++) {
 
-      if ( ((i+C) > last_lastblock) || (last_size != sb->s_blocks_count) ) {
-	if (!best_guess)
-	  printf("THESE ENTRIES ARE PROBABLY ALL FOR THE SAME PARTITION:\n");
-        last_lastblock = lastblock;
-        last_size = sb->s_blocks_count;
-	if ( (blocksize >= EXT2_MIN_BLOCK_SIZE) && (blocksize <= EXT2_MAX_BLOCK_SIZE) ) {
-	  if ( !(ranch_size) || ((ranch_size==last_size)&&(ranch_last==last_lastblock)) ) {
-            if (!best_guess)
-	      printf("**** I'D BET THE RANCH ON THIS NEXT ENTRY *************\n");
-            this_is_it = 1;
-	    ranch_size = last_size;
-	    ranch_last = last_lastblock;
+      sb = (void *)(buffer+j*512);
+#endif
+
+      /* It probably isn't it */
+      this_is_it = 0;
+    
+      /* Check if the ext2 magic number is in the right place */
+      /* if ( (sb->s_magic== EXT2_SUPER_MAGIC)||(sb->s_magic == EXT2_PRE_02B_MAGIC) ) { */
+      if (sb->s_magic== EXT2_SUPER_MAGIC) {
+
+	/* When we get a match, figure out where this partiton would end */
+	blocksize = (unsigned long)EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size;
+	/* END = START + (SIZE-1): i.e. 100 block fs starting at block 0 runs 0->99 */
+	lastblock = (i+C)+(sb->s_blocks_count*(blocksize/512UL)-1UL);
+
+	if ( ((i+C) > last_lastblock) || (last_size != sb->s_blocks_count) ) {
+	  if (!best_guess)
+	    printf("THESE ENTRIES ARE PROBABLY ALL FOR THE SAME PARTITION:\n");
+	  last_lastblock = lastblock;
+	  last_size = sb->s_blocks_count;
+	  if ( (blocksize >= EXT2_MIN_BLOCK_SIZE) && (blocksize <= EXT2_MAX_BLOCK_SIZE) ) {
+	    if ( !(ranch_size) || ((ranch_size==last_size)&&(ranch_last==last_lastblock)) ) {
+	      if (!best_guess)
+		printf("**** I'D BET THE RANCH ON THIS NEXT ENTRY *************\n");
+	      this_is_it = 1;
+	      ranch_size = last_size;
+	      ranch_last = last_lastblock;
+	    }
 	  }
 	}
+	if ((!best_guess)||(this_is_it)) {
+	  printf("   * Found ext2_magic in sector %lu (512 byte sectors).\n",i+C);
+	  printf("     This file system is %lu blocks long (each block is %lu bytes)\n",
+		 sb->s_blocks_count,blocksize);
+	  printf("     Filesystem runs %lu : %lu (512 byte sectors)\n",i+C,lastblock);
+	}
       }
-      if ((!best_guess)||(this_is_it)) {
-        printf("   * Found ext2_magic in sector %lu (512 byte sectors).\n",i+C);
-        printf("     This file system is %lu blocks long (each block is %lu bytes)\n",
-  	       sb->s_blocks_count,blocksize);
-        printf("     Filesystem runs %lu : %lu (512 byte sectors)\n",i+C,lastblock);
-      }
+      i++; /* Needs to be inside j loop */
+#if NB>1
     }
-
-    /* Be a wuss and increment it down here, it makes the code a little more readable */
-    i++;
+#endif
   }
 
   
