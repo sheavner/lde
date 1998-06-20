57513eabc8aa004348de0a76ee47a31d6437f749
sdh <sdh>
1998-06-20 17:47:35 +0000

    proto for MINIX_is_system_block

--- src/minix.h
+++ src/minix.h
@@ -3,13 +3,14 @@
  *
  *  Copyright (C) 1994  Scott D. Heavner
  *
- *  $Id: minix.h,v 1.4 1996/06/01 04:57:39 sdh Exp $
+ *  $Id: minix.h,v 1.5 1998/06/20 17:47:35 sdh Exp $
  */
 #ifndef LDE_MINIX_H
 #define LDE_MINIX_H
 
 extern int MINIX_inode_in_use(unsigned long inode_nr);
 extern int MINIX_zone_in_use(unsigned long inode_nr);
+extern int MINIX_is_system_block(unsigned long nr);
 extern unsigned long MINIX_map_inode(unsigned long nr);
 extern void MINIX_read_tables(void);
 extern void MINIX_init(void *sb_buffer);
